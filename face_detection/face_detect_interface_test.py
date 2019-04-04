7c70c0021b2bec6f36a0f79ec4b38b2f90ce6b6a
heroinlj <heroinlj@gmail.com>
2019-04-04 13:24:59 +0800

    添加ssd人脸检测接口和默认模型

---
+++ face_detection/face_detect_interface_test.py
@@ -0,0 +1,15 @@
+import os
+from face_detect_interface import FaceDetector, box_transform, draw_detection_rects
+import cv2
+working_root = os.path.split(os.path.realpath(__file__))[0]
+os.chdir(working_root)
+
+if __name__ == '__main__':
+    detector = FaceDetector()
+    image_path = os.path.join(os.getcwd(), "../samples/emma_input.jpg")
+    image = cv2.imread(image_path, 1)
+    bounding_boxes = detector.detect(image)
+    draw_detection_rects(image, bounding_boxes)
+    cv2.imshow("image", image)
+    cv2.waitKey()
+    print(bounding_boxes)

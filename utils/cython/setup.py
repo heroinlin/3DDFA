07fcb52a5a59903f2d1232cce01584c0190200ac
heroinlj <heroinlj@gmail.com>
2019-05-28 17:25:49 +0800

    mesh_core_cython在mac上的编译

--- utils/cython/setup.py
+++ utils/cython/setup.py
@@ -5,15 +5,16 @@ to compile
 
 # setup.py
 from distutils.core import setup, Extension
-# from Cython.Build import cythonize
 from Cython.Distutils import build_ext
 import numpy
 
 setup(
-    name='mesh_core_cython',
-    cmdclass={'build_ext': build_ext},
-    ext_modules=[Extension("mesh_core_cython",
-                           sources=["mesh_core_cython.pyx", "mesh_core.cpp"],
-                           language='c++',
-                           include_dirs=[numpy.get_include()])],
+name='mesh_core_cython',
+cmdclass={'build_ext': build_ext},
+ext_modules=[Extension("mesh_core_cython",
+sources=["mesh_core_cython.pyx", "mesh_core.cpp"],
+language='c++',
+extra_compile_args=['-stdlib=libc++', '-mmacosx-version-min=10.9','-std=c++11', '-D_hypot=hypot'],
+extra_link_args=['-stdlib=libc++', '-mmacosx-version-min=10.9','-std=c++11', '-D_hypot=hypot'],
+include_dirs=[numpy.get_include()])],
 )
